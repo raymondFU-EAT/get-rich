3c00418b4a902423f93460e643b1f32e530e1dab
raymondFU-EAT <79923758+raymondFU-EAT@users.noreply.github.com>
2021-03-03 10:41:30 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -14,7 +14,8 @@ price=65
 int=str
 str=False
 print(str, pen, rain, name)
-print(price*5, pen*5)
+print(price*5, pen*5, rain*5, name*5)
+print(rain+5)
 H=input('請輸入身高')
 W=input('請誠實輸入體重')
 Z=input('想賺多少錢')
